70ebe266a298f4f5e77f5b746d590bec0c99ecfe
rupanisreevani <rupanisreevani@gmail.com>
2026-02-02 13:10:31 +0530

    js

---
+++ problems/variables.js
@@ -0,0 +1,23 @@
+// Swap two variables without using a third variable
+
+// let a = 10;
+// let b = 20;
+
+// [a, b] = [b, a];
+
+// console.log(a, b);
+
+
+// Write a program to check whether a value is number or string.
+
+// let value =133
+// if (typeof value==="number"){
+//     console.log("it is a number")
+// }else if(typeof value==="string"){
+//     console.log("it is a string")
+// }
+
+// Write a program to find the largest of three numbers using operators.
+
+let a=10,b=25,c=15
+let largest = a>b && a>c 
